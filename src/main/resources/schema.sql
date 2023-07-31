4557d59cdb75ec7a5fe803e82898f54d44e6892a
eclipse1111 <eclipse.samp@yahoo.com>
2023-07-31 11:40:58 +0300

    Added schema for auth database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,62 @@
+CREATE TABLE IF NOT EXISTS active_user (
+    id bigint NOT NULL,
+    uid uuid NOT NULL,
+    username character varying NOT NULL,
+    password character varying NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS pending_user (
+    id bigint NOT NULL,
+    uid uuid NOT NULL,
+    username character varying NOT NULL,
+    password character varying NOT NULL,
+    email character varying NOT NULL,
+    created_at timestamp NOT NULL,
+    validation_code uuid NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_profile (
+    id bigint NOT NULL,
+    firstname character varying NOT NULL,
+    lastname character varying NOT NULL,
+    email character varying NOT NULL,
+    country character varying NOT NULL,
+    age SMALLINT NOT NULL,
+    user_id bigint NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+            REFERENCES active_user(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS "group" (
+    id bigint NOT NULL,
+    name character varying NOT NULL,
+    uid uuid NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_type (
+    id bigint NOT NULL,
+    name character varying NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS group_member (
+    id bigint NOT NULL,
+    user_id bigint NOT NULL,
+    user_type_id bigint NOT NULL,
+    group_id bigint NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+            REFERENCES active_user(id),
+    CONSTRAINT fk_user_type_id
+        FOREIGN KEY (user_type_id)
+            REFERENCES user_type(id),
+    CONSTRAINT fk_group_id
+        FOREIGN KEY (group_id)
+            REFERENCES "group"(id),
+    PRIMARY KEY (id)
+);
