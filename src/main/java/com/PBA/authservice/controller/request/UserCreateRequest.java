069cc5e520969b601c17ef60aff2d3638eed71a7
eclipse1111 <eclipse.samp@yahoo.com>
2023-08-25 12:46:39 +0300

    Added OpenAPI request examples

--- src/main/java/com/PBA/authservice/controller/request/UserCreateRequest.java
+++ src/main/java/com/PBA/authservice/controller/request/UserCreateRequest.java
@@ -1,5 +1,6 @@
 package com.pba.authservice.controller.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -17,6 +18,7 @@ public class UserCreateRequest {
     @NotBlank(message = "{username.notblank}")
     @Size(min = 4, message = "{username.minsize}")
     @Size(max = 20, message = "{username.maxsize}")
+    @Schema(example = "john_doe")
     private String username;
 
     @NotBlank(message = "{password.notblank}")
@@ -24,17 +26,21 @@ public class UserCreateRequest {
     @Size(max = 20, message = "{password.maxsize}")
     @Pattern(regexp = ".*[!@#$%^&*?].*", message = "{password.specialchar}")
     @Pattern(regexp = ".*[A-Z].*", message = "{password.uppercase}")
+    @Schema(example = "Password50!@")
     private String password;
 
     @NotBlank(message = "{firstname.notblank}")
     @Size(max = 50, message = "{firstname.maxsize}")
+    @Schema(example = "John")
     private String firstName;
 
     @NotBlank(message = "{lastname.notblank}")
     @Size(max = 50, message = "{lastname.maxsize}")
+    @Schema(example = "Doe")
     private String lastName;
 
     @NotBlank(message = "{email.notblank}")
     @Email(message = "{email.invalid}")
+    @Schema(example = "john_doe@gmail.com")
     private String email;
 }
